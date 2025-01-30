74a4cfde248f87bbbbd6293bb5aba8dfbdbb9421
AlexGreen <107873309+Alex-Green-Oleksiy@users.noreply.github.com>
2025-01-30 09:16:19 +0200

    Update task_01.js

--- script/lesson__3/scripts/task_01.js
+++ script/lesson__3/scripts/task_01.js
@@ -4,23 +4,19 @@
 // Вивести не екран ім’я тієї дитини, у якої кількість цукерок є більшою,
 // або вивести, що кількість однакова.
 
-
-
 //1Вводимо дані
-const nameFirstChild = prompt("name1", "Jak");//імена дітей
-const nameSecondChild = prompt("name2", "Grase");//імена дітей
+const nameFirstChild = prompt("Введіть ім'я першої дитини", "Jak");//імена дітей
+const nameSecondChild = prompt("Введіть ім'я другої дитини", "Grase");//імена дітей
 
-const quantityCandyFirst = parseFloat(
-    prompt(`Кількість цукерок у ${nameFirstChild}`, "5")
-);//кількість цукурок
-const quantityCandySecond = parseFloat(
-    prompt(`Кількість цукерок у ${nameSecondChild}`, "7")
-);//кількість цукурок
-//2Порівнюємо і виводимо результат
-if (quantityCandyFirst > quantityCandySecond) {
-    document.write(`Winner = ${nameFirstChild}`);
+const quantityCandyFirst = parseFloat(prompt(`Кількість цукерок у ${nameFirstChild}`, "5"));//кількість цукурок
+const quantityCandySecond = parseFloat(prompt(`Кількість цукерок у ${nameSecondChild}`, "7"));//кількість цукурок
+//2Порівнюємо перевіряємо на некорректний ввод і виводимо результат
+if (isNaN(quantityCandyFirst) || isNaN(quantityCandySecond)) {
+    document.write("Помилка введення, введіть число");
 } else if (quantityCandyFirst < quantityCandySecond) {
-    document.write(`Winner = ${nameSecondChild}`);
+    document.write(`Переможець = ${nameSecondChild}, у неї більше цукерок на ${quantityCandySecond - quantityCandyFirst}`);
+} else if (quantityCandyFirst > quantityCandySecond) {
+    document.write(`Переможець = ${nameFirstChild}, у нього більше цукерок на ${quantityCandyFirst - quantityCandySecond}`);
 } else {
-    document.write(`Winner = Friends`);
+    document.write("Переможець = Обидва друзі, у них однакова кількість цукерок");
 }
