750072e9e016b7f6c29a43c74a1a7c0d6fcdc1fa
AlexGreen <107873309+Alex-Green-Oleksiy@users.noreply.github.com>
2025-01-30 09:24:58 +0200

    si

--- script/lesson__3/scripts/task_01.js
+++ script/lesson__3/scripts/task_01.js
@@ -5,18 +5,32 @@
 // або вивести, що кількість однакова.
 
 //1Вводимо дані
-const nameFirstChild = prompt("Введіть ім'я першої дитини", "Jak");//імена дітей
-const nameSecondChild = prompt("Введіть ім'я другої дитини", "Grase");//імена дітей
+const nameFirstChild = prompt("Введіть ім'я першої дитини", "Jak"); //імена дітей
+const nameSecondChild = prompt("Введіть ім'я другої дитини", "Grase"); //імена дітей
 
-const quantityCandyFirst = parseFloat(prompt(`Кількість цукерок у ${nameFirstChild}`, "5"));//кількість цукурок
-const quantityCandySecond = parseFloat(prompt(`Кількість цукерок у ${nameSecondChild}`, "7"));//кількість цукурок
+const quantityCandyFirst = parseFloat(
+    prompt(`Кількість цукерок у ${nameFirstChild}`, "5")
+); //кількість цукурок
+const quantityCandySecond = parseFloat(
+    prompt(`Кількість цукерок у ${nameSecondChild}`, "7")
+); //кількість цукурок
 //2Порівнюємо перевіряємо на некорректний ввод і виводимо результат
 if (isNaN(quantityCandyFirst) || isNaN(quantityCandySecond)) {
     document.write("Помилка введення, введіть число");
 } else if (quantityCandyFirst < quantityCandySecond) {
-    document.write(`Переможець = ${nameSecondChild}, у неї більше цукерок на ${quantityCandySecond - quantityCandyFirst}`);
+    document.write(
+        `Переможець = ${nameSecondChild}, у цієї дитини більше цукерок на ${
+            quantityCandySecond - quantityCandyFirst
+        }`
+    );
 } else if (quantityCandyFirst > quantityCandySecond) {
-    document.write(`Переможець = ${nameFirstChild}, у нього більше цукерок на ${quantityCandyFirst - quantityCandySecond}`);
+    document.write(
+        `Переможець = ${nameFirstChild}, у цієї дитини більше цукерок на ${
+            quantityCandyFirst - quantityCandySecond
+        }`
+    );
 } else {
-    document.write("Переможець = Обидва друзі, у них однакова кількість цукерок");
+    document.write(
+        "Переможець = Обидва друзі, у них однакова кількість цукерок"
+    );
 }
